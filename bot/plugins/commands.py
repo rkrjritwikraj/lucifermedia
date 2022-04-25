570e76f6ef2af887ae5becfda478940b8f4f2998
SILENTKILLER_TG <90544826+rkrjritwikraj@users.noreply.github.com>
2022-04-25 16:11:31 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -10,10 +10,10 @@ from bot.translation import Translation # pylint: disable=import-error
 async def start(bot, update):
     
     buttons = [[
-        InlineKeyboardButton('My Dev 👨‍🔬', url='https://t.me/AlbertEinstein_TG'),
-        InlineKeyboardButton('Source Code 🧾', url ='https://github.com/AlbertEinsteinTG/Adv-Auto-Filter-Bot')
+        InlineKeyboardButton('My Dev 👨‍🔬', url='https://t.me/silentkilleroff'),
+        InlineKeyboardButton('Source Code 🧾', url ='https://github.com/rkrjritwikraj/lucifermedia')
     ],[
-        InlineKeyboardButton('Support 🛠', url='https://t.me/CrazyBotszGrp')
+        InlineKeyboardButton('Support 🛠', url='https://t.me/the_mallu_casino')
     ],[
         InlineKeyboardButton('Help ⚙', callback_data="help")
     ]]
